5e5f95d61d0bdf1023c43a040d96353534d11efe
Amador Cuenca <sphi02ac@gmail.com>
2022-03-22 20:58:30 -0300

    feat: Prototype the three main pages

---
+++ src/pages/AuthorPage.js
@@ -0,0 +1,10 @@
+import { React } from 'react';
+
+
+export const AuthorPage = () => {
+    return (
+        <div className="AuthorPage">
+            <h1>Author Page</h1>
+        </div>
+    );
+}
---
+++ src/pages/BookPage.js
@@ -0,0 +1,10 @@
+import { React } from 'react';
+
+
+export const BookPage = () => {
+    return (
+        <div className="BookPage">
+            <h1>Book Page</h1>
+        </div>
+    );
+}
